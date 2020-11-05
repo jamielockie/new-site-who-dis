d1173ba25119bdc09e0cc2ba2b42e8424ba05b5c
Jamie <jamielockie@gmail.com>
2020-11-04 19:03:12 -0500

    Update component paths

--- src/components/waves.js
+++ src/components/waves.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const Waves = ({ className }) => (
+export const waves = ({ className }) => (
   <svg
     className={className}
     xmlns="http://www.w3.org/2000/svg"
